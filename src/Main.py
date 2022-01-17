ae331cd70e9e79d7a0cf619e4af67b13ef06f321
mariamhap-py <95041043+mariamhap-py@users.noreply.github.com>
2022-01-17 12:54:59 +0100

    Update Main.py

--- src/Main.py
+++ src/Main.py
@@ -14,15 +14,19 @@ from House import House
 from Opening import Opening
 import copy
 
+import pygame
+import OpenGL.GL as gl
+import OpenGL.GLU as glu
 
 def Q1a():
-    pass
+    return Configuration()
     
 def Q1b_f():
     return Configuration({'screenPosition': -5, 'xAxisColor': [1, 1, 0]}). \
         setParameter('xAxisColor', [1, 1, 0]). \
         setParameter('yAxisCo lor', [0,1,1]). \
         display()
+        
         
 def Q2b():
     # Ecriture en utilisant le chaînage
@@ -37,14 +41,17 @@ def Q2c():
             )
 
 def Q3a():
-    pass  
+    #construction d'un mur
+    return Configuration().add(
+            Wall({'position': [1, 1, 0], 'width':7, 'height':2.6, 'orientation': 90})
+            )
 
 def Q4a():
-    # Ecriture en utilisant des variables : A compléter
-    wall1 = Wall(...)
-    wall2 = Wall(...)
-    wall3 = Wall(...)
-    wall4 = Wall(...)  
+    # Construction d'une maison de 4 murs, épaisseur (thickness) choisie par défaut
+    wall1 = Wall({'position': [1, 1, 0], 'width':7, 'height':5, 'orientation': 0})
+    wall2 = Wall({'position': [1, 1, 0], 'width':7, 'height':5, 'orientation': 90})
+    wall3 = Wall({'position': [1, 1, 0], 'width':7, 'height':5, 'orientation': 90})
+    wall4 = Wall({'position': [1, 1, 0], 'width':7, 'height':5, 'orientation': 0})  
     house = House({'position': [-3, 1, 0], 'orientation':0})
     house.add(wall1).add(wall3).add(wall4).add(wall2)
     return Configuration().add(house)   
@@ -67,7 +74,8 @@ def Q5b():
     print(section.canCreateOpening(opening3))
     return Configuration()    
     
-def Q5c1():      
+def Q5c1(): 
+    #creation d'une ouverture de porte     
     section = Section({'width':7, 'height':2.6})
     opening1 = Opening({'position': [2, 0, 0], 'width':0.9, 'height':2.15, 'thickness':0.2, 'color': [0.7, 0.7, 0.7]})
     sections = section.createOpening(opening1)
@@ -76,7 +84,8 @@ def Q5c1():
         configuration.add(x)    
     return configuration     
     
-def Q5c2():      
+def Q5c2():  
+#creation d'une ouverture de fenêtre    
     section = Section({'width':7, 'height':2.6})
     opening2 = Opening({'position': [4, 0, 1.2], 'width':1.25, 'height':1, 'thickness':0.2, 'color': [0.7, 0.7, 0.7]}) 
     sections = section.createNewSections(opening2)
@@ -86,7 +95,10 @@ def Q5c2():
     return configuration    
 
 def Q5d():      
-    pass
+    wall = Wall({'width':7, 'height':2.6,})
+    opening1 = Opening({'position': [2, 0, 0], 'width':0.9, 'height':2.15, 'thickness':0.2, 'color': [0.7, 0.7, 0.7]})
+    section = wall.findSection(opening1)
+    print(section)
     
 def Q6():  
     pass  
@@ -94,17 +106,17 @@ def Q6():
 def main():
     # Enlever un des commentaires pour la question traitée
     
-    configuration = Q1a()
-    # configuration = Q1b_f()
-    # configuration = Q2b()
+    #configuration = Q1a()
+    #configuration = Q1b_f()
+    #configuration = Q2b()
     # configuration = Q2c()
-    # configuration = Q3a()
-    # configuration = Q4a()
+    #configuration = Q3a()
+    #configuration = Q4a()
     # configuration = Q5a()
     # configuration = Q5b()
     # configuration = Q5c1()
     # configuration = Q5c2() 
-    # configuration = Q5d()
+    configuration = Q5d()
     # configuration = Q6()
     configuration.display()     
          
