b26798d0cd3e425dc9a6f229d001e57e4902fd18
mariamhap-py <95041043+mariamhap-py@users.noreply.github.com>
2022-01-17 17:32:26 +0100

    Update Section.py

--- src/Section.py
+++ src/Section.py
@@ -91,18 +91,25 @@ class Section:
     #peut être ajoutée dans la section, représentée par self, et False sinon
     def canCreateOpening(self, x):
         #on compare la position et la taille de chacune des 3 dimensions
-        #position de x + longueur
-        if self.parameters['position'][0]<x.parameters['position'][0]:
-            
-            if self.parameters['position'][0]+self.parameters['width']<self.parameters['position'][0]+x.parameters['width']:
-        #position de y + épaisseur
-               if self.parameters['position'][1]<x.parameters['position'][1]:
-                   if self.parameters['position'][1]+self.parameters['thickness']<x.parameters['position'][1]+x.parameters['thickness']:
-                       #position de z + hauteur
-                       if self.parameters['position'][2]<x.parameters['position'][2]:
-                           if self.parameters['position'][2]+self.parameters['height']<x.parameters['position'][2]+x.parameters['height']:
-                               return True
-        return False
+        return (self.parameters["width"] >= x.parameters["width"] 
+                and self.parameters["height"] >= x.parameters["height"] 
+                and self.parameters["position"][1] == x.parameters["position"][1] 
+                and self.parameters["position"][0] <= x.parameters["position"][0] 
+                and self.parameters["position"][0] + self.parameters["width"]>= x.parameters["position"][0] + x.parameters["width"] 
+                and self.parameters["position"][2] <= x.parameters["position"][2] 
+                and self.parameters["position"][2] + self.parameters["height"]>= x.parameters["position"][2]+ x.parameters["height"]) 
+#        #position de x + longueur
+#        if self.parameters['position'][0]<x.parameters['position'][0]:
+#            
+#            if self.parameters['position'][0]+self.parameters['width']>x.parameters['position'][0]+x.parameters['width']:
+#        #position de y + épaisseur
+#               if self.parameters['position'][1]<x.parameters['position'][1]:
+#                   if self.parameters['position'][1]+self.parameters['thickness']>x.parameters['position'][1]+x.parameters['thickness']:
+#                       #position de z + hauteur
+#                       if self.parameters['position'][2]<x.parameters['position'][2]:
+#                           if self.parameters['position'][2]+self.parameters['height']>x.parameters['position'][2]+x.parameters['height']:
+#                               return True
+#        return False
                    
         
     # Creates the new sections for the object x
@@ -110,51 +117,80 @@ class Section:
     def createNewSections(self, x):
         #on initialise la liste de sortie
         rep = [] 
-        #une ouverture va créer 4 sections dont 
+        
+        #on simplifie les noms des parametres de la section
+        position=self.parameters['position'] 
+        width=self.parameters['width'] 
+        height=self.parameters['height'] 
+        thickness=self.parameters['thickness'] 
+        orientation=self.parameters['orientation'] 
+        edges=self.parameters['color'] 
+        color=self.parameters['color'] 
+
+        if self.canCreateOpening(x): 
+          if x.getParameter('position')[0]>position[0]: 
+              #une ouverture va créer 4 sections dont 
         #l'epaisseur sera la même que celle de la section initiale
+
+            sect1= Section({'position': position, 
+
+            'width': x.getParameter('position')[0]-position[0], 
+            'height': height, 
+            'thickness': thickness})   
+            rep.append(sect1) 
+
+          if (x.getParameter('position')[2] + x.getParameter('height')!=height+position[2]): 
+
+             
+
+              sect2=Section({ 
+
+              'position': [x.getParameter('position')[0], position[1],x.getParameter('position')[2]+x.getParameter('height')], 
+              'width': x.getParameter('width'), 
+              'height': position[2]+height - (x.getParameter('position')[2]+x.getParameter('height')), 
+              'thickness': thickness, 
+              'orientation': orientation, 
+              'edges':edges, 
+              'color': color}) 
+
+              rep.append(sect2) 
+
+
+          if (x.getParameter('position')[2]>position[2]): 
+
+            sect3=Section({ 
+
+            'position': [x.getParameter('position')[0], position[1],position[2]], 
+            'width': x.getParameter('width'), 
+            'height': x.getParameter('position')[2]-position[2], 
+            'thickness': thickness, 
+            'orientation': orientation, 
+            'edges':edges, 
+            'color': color}) 
+            rep.append(sect3) 
+
+           
+          if (width+position[0]>(x.getParameter('position')[0]+x.getParameter('width'))): 
+
+            sect4= Section({ 
+
+            'position': [x.getParameter('position')[0]+x.getParameter('width'), position[1],position[2]], 
+            'width': position[0]+width-(x.getParameter('position')[0]+x.getParameter('width')), 
+            'height': height, 
+            'thickness': thickness, 
+            'orientation': orientation, 
+            'edges':edges, 
+            'color': color}) 
+            rep.append(sect4) 
+
+          
+        return rep     
         
-        #section de gauche
-        section1=Section({'position':[self.parameters['position'][0],
-                                      self.parameters['position'][1],
-                                      self.parameters['position'][2]],
-                        'width':x.parameters['position'][0]-self.parameters['position'][0],
-                        'thickness':self.parameters['thickness'],
-                        'height':self.parameters['height']})
-        rep.append(section1)
-        
-        #section du haut
-        section2=Section({'position':[x.parameters['position'][0],
-                                      self.parameters['position'][1],
-                                      self.parameters['position'][2]],
-                        'width':x.parameters['width'],
-                        'thickness':self.parameters['thickness'],
-                        'height':x.parameters['position'][2]-self.parameters['position'][2]})
-        rep.append(section2)
-        
-        #section du bas
-        section3=Section({'position':[x.parameters['position'][0],
-                                      self.parameters['position'][1],
-                                      x.parameters['position'][2]+x.parameters['height']],
-                        'width':x.parameters['width'],
-                        'thickness':self.parameters['thickness'],
-                        'height':x.parameters['height']-x.parameters['position'][2]})
-        rep.append(section3)
-        
-        #section de droite
-        section4=Section({'position':[x.parameters['position'][0]+x.parameters['width'],
-                                      self.parameters['position'][1],
-                                      self.parameters['position'][2]],
-                        'width':self.parameters['width']-x.parameters['width']+x.parameters['position'][0],
-                        'thickness':self.parameters['thickness'],
-                        'height':self.parameters['height']})
-        rep.append(section4)
-        
-        return rep            
         
     # Tracer les arêtes
     def drawEdges(self):
         gl.glPushMatrix()
-        gl.glTranslate(self.parameters['position'][0],
+        gl.glTranslatef(self.parameters['position'][0],
                        self.parameters['position'][1], 
                        self.parameters['position'][2])
         gl.glPolygonMode(gl.GL_FRONT_AND_BACK,gl.GL_LINE) # on trace les faces : GL_FILL
@@ -194,5 +230,8 @@ class Section:
         gl.glEnd()
         gl.glPopMatrix()
        
+  
+  
+
   
   
